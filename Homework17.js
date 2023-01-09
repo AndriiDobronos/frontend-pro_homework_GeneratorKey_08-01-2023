be798787df4ded85cb31e8bd8833d2c0820e53e3
Andrii Dobronos <andriidobronos@gmail.com>
2023-01-09 13:02:52 +0200

    enhanced code

--- Homework17.js
+++ Homework17.js
@@ -5,21 +5,23 @@
 //Наприклад:
 //console.log(generateKey(length,characters))
 //console.log(key); // eg599gb60q926j8i
-const characters = (['abcdefghijklmnopqrstuvwxyz0123456789']).join(",")
+//const characters = (['abcdefghijklmnopqrstuvwxyz0123456789']).join(",")
+//const characters = Array.from('abcdefghijklmnopqrstuvwxyz0123456789')
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
 const length = 16
 const sourceArr = []
 const key = generateKey(length, characters)
 function generateKey(length,characters) {
     let j = 0
     while (j < length) {
-        const numberPosition = 36
-        function getRandomInt(numberPosition) {
+        const numberPosition = Array.from(characters).length
+        function getRandomPosition(numberPosition) {
             return Math.floor(Math.random() * numberPosition)
         }
-        sourceArr.push(characters[Math.floor(Math.random() * numberPosition)])
+        sourceArr.push(Array.from(characters)[Math.floor(Math.random() * numberPosition)])
         j = j + 1
     }
     return sourceArr.join('')
 }
-//alert(generateKey(length,characters))
+alert(generateKey(key))
 console.log(key)
